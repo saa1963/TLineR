71e4b5136b950b1866c11d8e15ad3ebd72f25348
Андрей Сошин <a_soshin@mail.ru>
2020-06-10 16:46:19 +0300

    11

--- ClientApp/src/components/NavMenu.tsx
+++ ClientApp/src/components/NavMenu.tsx
@@ -1,42 +1,52 @@
-import * as React from 'react';
-import { Collapse, Container, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from 'reactstrap';
-import { Link } from 'react-router-dom';
+﻿import * as React from 'react';
+import { Collapse, Container, Navbar, NavbarToggler, DropdownToggle, DropdownMenu, DropdownItem, Dropdown, Button } from 'reactstrap';
 import './NavMenu.css';
 
-export default class NavMenu extends React.PureComponent<{}, { isOpen: boolean }> {
-    public state = {
-        isOpen: false
-    };
+export default class NavMenu extends React.PureComponent<{}, { isOpen: boolean, dropdownOpen: boolean }> {
+  public state = {
+    isOpen: false,
+    dropdownOpen: false
+  };
 
-    public render() {
-        return (
-            <header>
-                <Navbar className="navbar-expand-sm navbar-toggleable-sm border-bottom box-shadow mb-3" light>
-                    <Container>
-                        <NavbarBrand tag={Link} to="/">TLineR</NavbarBrand>
-                        <NavbarToggler onClick={this.toggle} className="mr-2"/>
-                        <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={this.state.isOpen} navbar>
-                            <ul className="navbar-nav flex-grow">
-                                <NavItem>
-                                    <NavLink tag={Link} className="text-dark" to="/">Home</NavLink>
-                                </NavItem>
-                                <NavItem>
-                                    <NavLink tag={Link} className="text-dark" to="/counter">Counter</NavLink>
-                                </NavItem>
-                                <NavItem>
-                                    <NavLink tag={Link} className="text-dark" to="/fetch-data">Fetch data</NavLink>
-                                </NavItem>
-                            </ul>
-                        </Collapse>
-                    </Container>
-                </Navbar>
-            </header>
-        );
-    }
+  public render() {
+    return (
+      <header>
+        <Navbar className="navbar-expand-sm navbar-toggleable-sm border-bottom box-shadow mb-3" light>
+            <Button className="nbutton mr-1" id="prev_page" color="primary" href="#">&lt;&lt;</Button>
+            <Button className="nbutton mr-1" id="prev_period" color="primary">&lt;</Button>
+            <Button className="nbutton mr-1" id="next_period" color="primary" href="#">&gt;</Button>
+            <Button className="nbutton" id="next_page" color="primary" href="#">&gt;&gt;</Button>
+            <NavbarToggler onClick={this.toggle} className="mr-2" />
+            <Collapse className="d-sm-inline-flex flex-sm-row" isOpen={this.state.isOpen} navbar>
+              <Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggleDropdown}>
+                <DropdownToggle nav caret>
+                  Линия времени
+                </DropdownToggle>
+                <DropdownMenu>
+                  <DropdownItem>Новая</DropdownItem>
+                  <DropdownItem>Загрузить</DropdownItem>
+                  <DropdownItem>Загрузить файл</DropdownItem>
+                </DropdownMenu>
+            </Dropdown>
+            <Container className="d-flex justify-content-end">
+              <Button color="primary" className="mr-1" outline>Регистрация</Button>
+              <Button color="primary" outline>Вход</Button>
+            </Container>
+            </Collapse>
+        </Navbar>
+      </header>
+    );
+  }
 
-    private toggle = () => {
-        this.setState({
-            isOpen: !this.state.isOpen
-        });
-    }
+  private toggle = () => {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+
+  private toggleDropdown = () => {
+    this.setState({
+      dropdownOpen: !this.state.dropdownOpen
+    });
+  }
 }
