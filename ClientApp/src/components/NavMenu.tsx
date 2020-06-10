625f7c47b8f5a04df317411cc7127291e7d9e239
Андрей Сошин <a_soshin@mail.ru>
2020-06-10 22:59:11 +0300

    1212

--- ClientApp/src/components/NavMenu.tsx
+++ ClientApp/src/components/NavMenu.tsx
@@ -12,27 +12,30 @@ export default class NavMenu extends React.PureComponent<{}, { isOpen: boolean, 
     return (
       <header>
         <Navbar className="navbar-expand-sm navbar-toggleable-sm border-bottom box-shadow mb-3" light>
+          <Container className="justify-content-start">
             <Button className="nbutton mr-1" id="prev_page" color="primary" href="#">&lt;&lt;</Button>
             <Button className="nbutton mr-1" id="prev_period" color="primary">&lt;</Button>
             <Button className="nbutton mr-1" id="next_period" color="primary" href="#">&gt;</Button>
             <Button className="nbutton" id="next_page" color="primary" href="#">&gt;&gt;</Button>
-            <NavbarToggler onClick={this.toggle} className="mr-2" />
-            <Collapse className="d-sm-inline-flex flex-sm-row" isOpen={this.state.isOpen} navbar>
-              <Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggleDropdown}>
-                <DropdownToggle nav caret>
-                  Линия времени
+          </Container>
+
+          <Collapse className="d-sm-inline-flex flex-sm-row" isOpen={this.state.isOpen} navbar>
+            <Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggleDropdown}>
+              <DropdownToggle nav caret>
+                Линия времени
                 </DropdownToggle>
-                <DropdownMenu>
-                  <DropdownItem>Новая</DropdownItem>
-                  <DropdownItem>Загрузить</DropdownItem>
-                  <DropdownItem>Загрузить файл</DropdownItem>
-                </DropdownMenu>
+              <DropdownMenu>
+                <DropdownItem>Новая</DropdownItem>
+                <DropdownItem>Загрузить</DropdownItem>
+                <DropdownItem>Загрузить файл</DropdownItem>
+              </DropdownMenu>
             </Dropdown>
-            <Container className="d-flex justify-content-end">
+            <Container fluid className="justify-content-end">
               <Button color="primary" className="mr-1" outline>Регистрация</Button>
               <Button color="primary" outline>Вход</Button>
             </Container>
-            </Collapse>
+          </Collapse>
+          <NavbarToggler onClick={this.toggle} className="mr-2" />
         </Navbar>
       </header>
     );
